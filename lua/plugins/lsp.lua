fc37dff263fc78d41f56e860504f347ecbfbed55
Callum Kloos <Callumkloos@gmail.com>
2025-03-19 12:27:39 +0000

    move setup into plugin initialisation for conform

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,4 +32,34 @@ return {
 			{ "nvim-treesitter/nvim-treesitter" },
 		},
 	},
+
+	{
+		"stevearc/conform.nvim",
+		opts = {
+			formatters = {
+				biome = {
+					require_cwd = true,
+				},
+				deno_fmt = {
+					cwd = require("conform.util").root_file { "deno.json" },
+					require_cwd = true,
+				},
+			},
+			formatters_by_ft = {
+				javascript = { "biome", "prettierd", stop_after_first = true },
+				typescript = { "biome", "deno_fmt", "prettierd", stop_after_first = true },
+				javascriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+				typescriptreact = { "biome", "prettierd", "prettier", stop_after_first = true },
+				css = { "prettierd" },
+				html = { "biome", "prettierd", stop_after_first = true },
+				astro = { "prettierd" },
+				json = { "biome", "prettierd", stop_after_first = true },
+				rust = { "rustfmt" },
+				go = { "gofmt" },
+				sh = { "shfmt" },
+				ruby = { "rubyfmt" },
+				gleam = { "gleam" },
+			},
+		},
+	},
 }
